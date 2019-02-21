b68e9b9046c66b692e3208c92341602186bdbe22
meisam <meisam@knights.ucf.edu>
2019-02-21 16:14:54 -0500

    Q3 in progress, need to construct matrices

--- CDA-5600/q3.py
+++ CDA-5600/q3.py
@@ -1,12 +1,13 @@
 import numpy as np
+import sys
 
 def contruct(d):
     pass
 
 
 def are_orthogonal(X, Z):
-    shape1 = np.shape(vec1)
-    shape2 = np.shape(vec2)
+    shape1 = np.shape(X)
+    shape2 = np.shape(Z)
 
     if shape1 != shape2:
         return False
@@ -16,4 +17,22 @@ def are_orthogonal(X, Z):
     # get D
     d = shape1[0]
 
+    for i in range(0, d):
+        x = X[i]
+        z = Z[i]
+        x = x.getH()        # take conjugate transpose of x
+        inner_product = np.matmul(x,z)
+        if inner_product != 0:
+            return False
+
     return True
+
+def main(dimension):
+    print("Dimension: %d" % (dimension) )
+    pass
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python q3.py dimension")
+        exit(0)
+    main(int(sys.argv[1]))
