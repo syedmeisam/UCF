c88303f601ee8cfbb089219dc05a8ba600c3032f
meisam <meisam@knights.ucf.edu>
2019-02-21 15:41:41 -0500

    Q3 started

---
+++ CDA-5600/q3.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def contruct(d):
+    pass
+
+
+def are_orthogonal(X, Z):
+    shape1 = np.shape(vec1)
+    shape2 = np.shape(vec2)
+
+    if shape1 != shape2:
+        return False
+    if shape1[0] != shape1[1]:
+        return False
+
+    # get D
+    d = shape1[0]
+
+    return True
