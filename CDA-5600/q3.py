9099eb77e050b0c061af2d2b01ef70bd2d93bd1b
syedmeisam <dev1@ofono.net>
2019-02-21 20:40:57 -0500

    Q3 done

--- CDA-5600/q3.py
+++ CDA-5600/q3.py
@@ -1,10 +1,6 @@
 import numpy as np
 import sys
 
-def contruct(d):
-    pass
-
-
 def are_orthogonal(X, Z):
     shape1 = np.shape(X)
     shape2 = np.shape(Z)
@@ -18,18 +14,34 @@ def are_orthogonal(X, Z):
     d = shape1[0]
 
     for i in range(0, d):
-        x = X[i]
-        z = Z[i]
-        x = x.getH()        # take conjugate transpose of x
-        inner_product = np.matmul(x,z)
-        if inner_product != 0:
-            return False
+        for j in range(0, d):
+            if (i == j):
+                continue
+            x = X[i]
+            z = Z[j]
+            x = np.transpose(x)        # take conjugate transpose of x
+            inner_product = np.matmul(x,z)
+            if inner_product != 0:
+                return False
 
     return True
 
-def main(dimension):
+def inner_product(dimension, a, b):
+    # a and b are the powers of the matrices, these are important because X is dependent on a
     print("Dimension: %d" % (dimension) )
-    pass
+    Z = np.identity(dimension)
+    X = np.identity(dimension)
+    Z = np.power(Z, b)
+
+    ket = [0]*(dimension)
+    for i in range(0, dimension):
+        ket[i] = (a+i) % dimension
+
+    print(ket)
+    print(are_orthogonal(X, Z))
+
+def main(dimension):
+    inner_product(dimension, 5, 1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
